c6fbe8edca5b698d4fd54e60779bb79167db2956
Zeth <zeth@zethjack.eu>
2022-11-22 21:19:20 +0100

    utility script to detect cores and adjust MAKEflag

---
+++ dot_local/bin/executable_cores.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo -ne "
+-------------------------------------------------------------------------
+                    You have " $nc" cores. And
+			changing the makeflags for "$nc" cores. Aswell as
+				changing the compression settings.
+-------------------------------------------------------------------------
+"
+TOTALMEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTALMEM -gt 8000000 ]]; then
+sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+fi
