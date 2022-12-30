0f93c9289c93eae2e15d354c73a86631ed75285e
Zeth <zeth@zethjack.eu>
2022-12-31 00:25:07 +0100

    Theming and retabbing

--- dot_config/awesome/theme.lua
+++ dot_config/awesome/theme.lua
@@ -11,16 +11,16 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "sans 8"
+theme.font          = "monospace 10"
 
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_focus      = "#ffa000"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
+theme.fg_normal     = "#ffa000"
+theme.fg_focus      = "#222222"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
