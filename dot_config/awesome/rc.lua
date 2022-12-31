0bb48cbc8c41894dbe34c97a8dc70b60a983402f
Zeth <zeth@zethjack.eu>
2022-12-31 08:25:04 +0100

    fixed missing icons.

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -19,7 +19,7 @@ local beautiful     = require("beautiful")
 local naughty       = require("naughty")
 local lain          = require("lain")
 --local menubar       = require("menubar")
-local freedesktop   = require("freedesktop")
+-- local freedesktop   = require("freedesktop")
 local hotkeys_popup = require("awful.hotkeys_popup")
                       require("awful.hotkeys_popup.keys")
 local mytable       = awful.util.table or gears.table -- 4.{0,1} compatibility
